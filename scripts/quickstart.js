f3a7d78491f73b5522faba25c48649b729498196
srivatsan <vatsan_k@hotmail.com>
2024-06-05 10:04:44 +0530

    Day 3 First Session

--- scripts/quickstart.js
+++ scripts/quickstart.js
@@ -24,13 +24,27 @@ obj.handleClick = function(){
 console.log("hello from method")
 event.preventDefault()
 }
+
+obj.clickHandler = function(){
+
+    return function(){
+        console.log("Inner function -Hello")
+      }
+    }
+
 obj.attachEvent = function(){
 
     var anchorTagRef = document.getElementById("firstLink")
 
+    // anchorTagRef.addEventListener('click',function(){
+
+    // })
     //anchorTagRef.addEventListener('click',this.handleClick())
 
     anchorTagRef.addEventListener('click',this.handleClick)
+ 
+    anchorTagRef.addEventListener('click',this.clickHandler())
+ 
 
 }
 obj.user = 'Ramesh'
