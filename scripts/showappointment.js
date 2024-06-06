3dc21ae92d9c92ce3b8f0ab040b2af30f0f4c878
srivatsan <vatsan_k@hotmail.com>
2024-06-06 14:31:33 +0530

    Post Lunch First Session

--- scripts/showappointment.js
+++ scripts/showappointment.js
@@ -21,7 +21,38 @@ appointment.createList = function(list,parentNode,listType){
 
 
 }
-let list = ['appointments must be booked One day Advance',
+
+appointment.createRow = function(data){
+
+    const tableRow = document.createElement("tr")
+
+    let colSize = Object.keys(data);
+
+    console.log(colSize)
+    console.log(colSize.length)
+        
+        let tableData = document.createElement('td')
+         tableData.textContent= data.doctorName
+
+         tableRow.appendChild(tableData)
+
+   return tableRow;
+
+}
+
+appointment.createTable = function(tableData)
+{
+    let tableRef = document.querySelector('table')
+    tableData.forEach((eachItem)=>{
+
+        let row =appointment.createRow(eachItem)
+        tableRef.append(row)
+
+    })
+     
+}
+    
+    let list = ['appointments must be booked One day Advance',
 'call  1800-2000-2000 for assistance']
 // let nameList = ['Ramesh',"suresh"]
 const parentNode = document.querySelector("article")
@@ -29,3 +60,7 @@ const parentNode = document.querySelector("article")
 appointment.createList(list,parentNode,'ol')
 
 // appointment.createList(nameList,parentNode,'ul')
+
+let tableData = [{slno:1,doctorName:'rakesh',timing:'weekday 7.00 pm and 8.00pm'}]
+
+appointment.createTable(tableData)
