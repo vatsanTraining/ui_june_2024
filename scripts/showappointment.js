dd2d824058beb215552077bac77a831b7768687b
srivatsan <vatsan_k@hotmail.com>
2024-06-06 13:05:50 +0530

    Day 4 Lunch Time

--- scripts/showappointment.js
+++ scripts/showappointment.js
@@ -2,15 +2,30 @@
 var appointment = {}
 
 
-appointment.createList = function(list){
 
-    // declare an array of appointment instructions
-    // access the article
-    //create ul 
-    // create number of li
-    // add to ul
-    //add ul to article
+appointment.createList = function(list,parentNode,listType){
 
+    const unordList = document.createElement(listType)
+
+    list.forEach(eachItem => {
+        
+        let childElement =document.createElement('li')
+
+           childElement.textContent=eachItem
+
+           unordList.appendChild(childElement)
+     
+    });
+
+    parentNode.appendChild(unordList);
 
 
 }
+let list = ['appointments must be booked One day Advance',
+'call  1800-2000-2000 for assistance']
+// let nameList = ['Ramesh',"suresh"]
+const parentNode = document.querySelector("article")
+
+appointment.createList(list,parentNode,'ol')
+
+// appointment.createList(nameList,parentNode,'ul')
