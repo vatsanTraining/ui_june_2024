68aae22d4680273bd0308a534a55ddc8db8571b3
srivatsan <vatsan_k@hotmail.com>
2024-06-06 10:03:10 +0530

    Day 4 Start

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,13 @@
 
 var obj ={}
 
-obj.createElement = function(tagName,attributes){
+obj.createElement = function(tagName,...attributes){
 
       element =document.createElement(tagName)
      
      attributes.forEach((eachElement,idx)=>{
 
+        console.log(eachElement)
               element.setAttribute(eachElement.attribKey,eachElement.attribValue)
 
      })
@@ -18,29 +19,51 @@ obj.createElement = function(tagName,attributes){
 
 obj.createHeader = function(){
 
-    let attribList = [{attribKey:'id',attribValue:'top-head'},
-                      {attribKey:'class',attribValue:'head-2x'}]
 
-    let element =obj.createElement('h1',attribList)
+    let element =obj.createElement('h1',{attribKey:'id',attribValue:'top-head'},
+                               {attribKey:'class',attribValue:'head-2x'})
 
-    element.innerHTML ='MedLife Hospitals'
+    element.textContent ='MedLife Hospitals'
 
     const headerElement = document.querySelector("#top-head")
 
     headerElement.appendChild(element)
 
+
+
+    let imgElement =obj.createElement('img',{attribKey:'src',attribValue:'images/logo.png'},
+    {attribKey:'class',attribValue:'min-width'},{attribKey:'alt',attribValue:"Company logo"})
+
+    headerElement.append(imgElement)
+
 }
 
 obj.createMenu =function(){
 
-    
-    let attribList = [{attribKey:'href',attribValue:'register.html'}]
- 
-     let element = obj.createElement('a',attribList);
+   
+    const parentNode =document.querySelector('#top-nav-bar')
 
-     element.textContent='Added By Javascript'
+    var attribList = [
+        {attribKey:'href',attribValue:'register.html',text:"Register"},
+        {attribKey:'href',attribValue:'bookappointment.html',text:"Appointment"},
+        {attribKey:'href',attribValue:'blog.html',text:"Blogs"},
+        {attribKey:'href',attribValue:'doctor.html',text:"Doctors"},
+        {attribKey:'href',attribValue:'contact.html',text:"Contact"}
 
-     document.querySelector('#top-nav-bar').appendChild(element)
+    ]
+
+    attribList.forEach((eachItem,idx)=>{
+
+        let newElement = document.createElement('a');
+
+        newElement.setAttribute(eachItem.attribKey,eachItem.attribValue)
+
+        newElement.textContent=eachItem.text
+
+        parentNode.appendChild(newElement)
+    })
+
+
 }
 
 
