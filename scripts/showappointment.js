af91feb62acaba8dc503865dc7ad196d2b604237
srivatsan <vatsan_k@hotmail.com>
2024-06-06 11:33:01 +0530

    Day 4 Session 1 commit

---
+++ scripts/showappointment.js
@@ -0,0 +1,16 @@
+
+var appointment = {}
+
+
+appointment.createList = function(list){
+
+    // declare an array of appointment instructions
+    // access the article
+    //create ul 
+    // create number of li
+    // add to ul
+    //add ul to article
+
+
+
+}
